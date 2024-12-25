1b9c5ae78065ed1366cb94994880ca956ea76814
Петр <Svbvalenok@mail.ru>
2024-12-26 01:48:49 +0300

    26.12.24

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Петр'
+title = 'Просмотр вебинара'
+content = 'Посмотреть видео с пропущенным вебинаром'
+status = 'Не выполнено'
+created_date = '26-12-2024'
+issue_date = '31-12-2024'
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки (Дедлайн):', issue_date)
